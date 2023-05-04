4595d6c14cfeaded7a46aacabf8fd203d7d4d93e
thanhmimi <thanh.nguyenph88@gmail.com>
2023-05-05 04:11:12 +0700

    add github action and new test

--- tests/upload_test.py
+++ tests/upload_test.py
@@ -16,4 +16,11 @@ class TestUploadGuru(object):
         upload_page.click_submit_button()
         assert upload_page.get_upload_result_msg() == "1 file\nhas been successfully uploaded."
 
+    def test_submit_without_file_upload(self, driver):
+        upload_page = UploadPage(driver)
+        driver.get(upload_page.page_url)
+        assert upload_page.is_loaded()
+        upload_page.click_submit_button()
+        assert upload_page.get_upload_result_msg() == "Please upload file before submitting..."
+
 
